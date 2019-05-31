dc90c12cb95c0b0d9fcbcf443b44e5efd8f239be
denistimkov <denistimkov@gmail.com>
2019-05-31 04:44:25 +0300

    init

---
+++ app/billing/views.py
@@ -0,0 +1,17 @@
+from aiohttp import web
+
+
+async def register(request):
+    return web.json_response({})
+
+
+async def make_deposit(request):
+    return web.json_response({})
+
+
+async def money_transfer(request):
+    return web.json_response({})
+
+
+async def quotes_upload(request):
+    return web.json_response({})
